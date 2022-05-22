a256ae844a16ae25c7106949ec998d8f5eea3d00
Suman Islam <sumanislam1416@gmail.com>
2022-05-22 17:52:27 +0600

    Implement nonNull fields and add resolver function for addUser mutation

--- schema/schema.js
+++ schema/schema.js
@@ -6,7 +6,8 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
-  GraphQLList
+  GraphQLList,
+  GraphQLNonNull
 } = graphql
 
 const CompanyType = new GraphQLObjectType({
@@ -68,19 +69,24 @@ const rootQuery = new GraphQLObjectType({
 
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
-  addUser: {
+  fields: {
+    addUser: {
     type: UserType,
     args: { 
-      firstName: { type: GraphQLString },
-      age: { type: GraphQLInt },
+      firstName: { type: new GraphQLNonNull(GraphQLString) },
+      age: { type: new GraphQLNonNull(GraphQLInt) },
       companyId: { type: GraphQLString }
     },
-    async resolve() {
-
+    async resolve(parentValue, args) {
+      const { firstName, age } = args;
+      const res = await axios.post('http://localhost:3000/users', {firstName, age});
+      return res.data;
     }
   }
-})
+  }
+});
 
 module.exports = new GraphQLSchema({
-  query: rootQuery
+  query: rootQuery,
+  mutation
 })
